4ea8237f8f7c23b948c16296c44e5b1835414fd4
Cory Boris <tromboris@gmail.com>
2023-01-24 16:46:23 -0500

    more game debugging

---
+++ memory_puzzle/game.rb
@@ -0,0 +1,48 @@
+require_relative "board"
+
+
+class Game
+
+    attr_reader :board, :prev_guess
+
+    def initialize
+        @board = Board.new()
+        @prev_guess = nil
+    end
+
+    def play
+        until @board.won?
+            system("clear")
+            @board.render
+            guess = get_guess
+            make_guess(guess)
+        end
+    end
+
+    def make_guess(guess)
+        if @prev_guess == nil
+            @prev_guess = @board[guess]
+            @prev_guess.reveal
+        else
+            this_guess = @board[guess]
+            this_guess.reveal
+            system("clear")
+            @board.render
+            if prev_guess.face_value != this_guess.face_value
+                puts "Try Again"
+                @prev_guess.hide
+                this_guess.hide
+            else
+                puts "It's a match!"
+            end
+            @prev_guess = nil
+        end
+    end
+
+    def get_guess
+        puts "Please enter the position of the card you would like to flip (e.g., (1, 2))"
+        guess = gets.chomp.split(",").map(&:to_i)
+    end
+
+
+end
